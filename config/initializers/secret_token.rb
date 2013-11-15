139a4656bd4d11814679197aedd2f413f51d7ab6
drsiegfried <barmyb_99@yahoo.com>
2013-11-15 15:58:47 +0000

    scafolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppOne::Application.config.secret_key_base = 'd80b9855be3a9a1509ed8f49203b8270e48f5829e83bdb9ec6d2e6fb0465112b79f95c5993d3d76f49455815da87319aa21884d138536f4e62983d5dacf5202c'
